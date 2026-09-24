cefa4d21ae16b8cadad933cfcfc2f173b856e180
jrodash2 <61074808+jrodash2@users.noreply.github.com>
2026-09-24 09:41:22 -0600

    Mostrar todos los programas en Formulario 1H

--- upcv_app/almacen_app/tests.py
+++ upcv_app/almacen_app/tests.py
@@ -1,9 +1,67 @@
 from datetime import date, datetime, timezone as datetime_timezone
 from decimal import Decimal
 
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 
+from .form import Form1hForm
+from .models import Dependencia, Programa
 from .templatetags.moneda_filters import fecha_corta, quetzales
+
+
+class Form1hFormTests(TestCase):
+    def test_preselecciona_primera_dependencia_y_programa_disponibles(self):
+        primera_dependencia = Dependencia.objects.create(
+            nombre="Primera", activo=False
+        )
+        Dependencia.objects.create(nombre="Segunda")
+        primera_programa = Programa.objects.create(nombre="Primero", activo=False)
+        Programa.objects.create(nombre="Segundo")
+
+        formulario = Form1hForm()
+
+        self.assertEqual(
+            formulario.fields["dependencia"].initial, primera_dependencia
+        )
+        self.assertEqual(formulario.fields["programa"].initial, primera_programa)
+
+    def test_muestra_todas_las_opciones_disponibles(self):
+        dependencia_inactiva = Dependencia.objects.create(
+            nombre="Inactiva", activo=False
+        )
+        dependencia_activa = Dependencia.objects.create(nombre="Activa")
+        programa_inactivo = Programa.objects.create(nombre="Inactivo", activo=False)
+        programa_activo = Programa.objects.create(nombre="Activo")
+
+        formulario = Form1hForm()
+
+        self.assertEqual(
+            list(formulario.fields["dependencia"].queryset),
+            [dependencia_inactiva, dependencia_activa],
+        )
+        self.assertEqual(
+            list(formulario.fields["programa"].queryset),
+            [programa_inactivo, programa_activo],
+        )
+
+    def test_respeta_valores_iniciales_existentes(self):
+        primera_dependencia = Dependencia.objects.create(nombre="Primera")
+        dependencia_inicial = Dependencia.objects.create(nombre="Inicial")
+        primer_programa = Programa.objects.create(nombre="Primero")
+        programa_inicial = Programa.objects.create(nombre="Inicial")
+
+        formulario = Form1hForm(
+            initial={
+                "dependencia": dependencia_inicial,
+                "programa": programa_inicial,
+            }
+        )
+
+        self.assertEqual(formulario.initial["dependencia"], dependencia_inicial)
+        self.assertEqual(formulario.initial["programa"], programa_inicial)
+        self.assertNotEqual(
+            formulario.initial["dependencia"], primera_dependencia
+        )
+        self.assertNotEqual(formulario.initial["programa"], primer_programa)
 
 
 class FormatoVisualTests(SimpleTestCase):
